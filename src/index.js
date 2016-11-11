58fa75a8e715aa1c1ee38f4ef58390cfcc85ab5a
Unknown <tomy_salminen@msn.com>
2016-11-11 18:52:44 +0200

    Modified project structure.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+import React, {
+  Component,
+} from 'react';
+
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Hello world!</Text>
+      </View>
+    );
+  }
+}
