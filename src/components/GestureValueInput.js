9adc5b8ec4636cdc954969567c45ac0f515b5e15
Unknown <tomy_salminen@msn.com>
2016-11-11 20:31:07 +0200

    Added preliminary gesture recognizer.

--- src/components/GestureValueInput.js
+++ src/components/GestureValueInput.js
@@ -7,6 +7,8 @@ import {
   View,
   Text,
 } from 'react-native';
+import {createResponder} from 'react-native-gesture-responder';
+
 import global from '../style';
 
 
@@ -15,10 +17,42 @@ export default class GestureValueInput extends Component {
     super(props);
   }
 
+  componentWillMount() {
+    this.gestureResponder = createResponder({
+      onStartShouldSetResponder: (evt, gestureState) => true,
+      onStartShouldSetResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetResponder: (evt, gestureState) => true,
+      onMoveShouldSetResponderCapture: (evt, gestureState) => true,
+      onResponderGrant: (evt, gestureState) => {},
+      onResponderMove: (evt, gestureState) => {console.log("1");},
+      onResponderTerminationRequest: (evt, gestureState) => true,
+      onResponderRelease: (evt, gestureState) => {console.log("2");},
+      onResponderTerminate: (evt, gestureState) => {console.log("3")},
+
+      onResponderSingleTapConfirmed: (evt, gestureState) => {console.log("4")},
+
+      moveThreshold: 2,
+      debug: false,
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text>Gesture valueinput</Text>
+      <View
+        {...this.gestureResponder}
+        style={{
+          backgroundColor: "rgb(90, 108, 19)",
+          width: 100,
+          height: 100,
+        }}
+      >
+        <Text
+          style={{
+            backgroundColor: "rgb(161, 57, 67)"
+          }}
+        >
+          Gesture valueinput
+        </Text>
       </View>
     );
   }
